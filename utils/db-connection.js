59c11e5d9f42e72e14eab5c01d6d7f715c92d16b
Zafir <zafir.alvi@northsouth.edu>
2022-07-31 04:22:26 +0600

    heroku config added

--- utils/db-connection.js
+++ utils/db-connection.js
@@ -1,9 +1,16 @@
 const Sequelize = require("sequelize");
 
-const PGHOST = "localhost";
-const PGDATABASE = "signal_dispatch_handler";
-const PGUSER = "postgres";
-const PGPASSWORD = "postgres";
+// //local
+// const PGHOST = "localhost";
+// const PGDATABASE = "signal_dispatch_handler";
+// const PGUSER = "postgres";
+// const PGPASSWORD = "postgres";
+
+//heroku
+const PGHOST = "ec2-18-214-134-226.compute-1.amazonaws.com";
+const PGDATABASE = "d6k137b9135j9b";
+const PGUSER = "xdizqtwlqqejgp";
+const PGPASSWORD = "5ec63913d6f457c3af86187215596812f4b41f4299d9fbd27b580df0504a4339";
 
 const sequelize = new Sequelize(PGDATABASE, PGUSER, PGPASSWORD, {
   host: PGHOST,
