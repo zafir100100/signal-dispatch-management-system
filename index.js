0080d9cc8fbb4b50f8071310406ee828a926db2f
Zafir <zafir.alvi@northsouth.edu>
2022-07-30 17:21:01 +0600

    route service name fixed

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ const express = require("express");
 const app = express();
 const port = 8090;
 const cors = require("cors");
-const userService = require("./repositories/user-info-repository");
-const transitSlipService = require("./repositories/transit-slip-repository");
-const despatchEnvelopService = require("./repositories/despatch-envelop-repository");
+const userService = require("./controllers/user-info-controller");
+const transitSlipService = require("./controllers/transit-slip-controller");
+const despatchEnvelopService = require("./controllers/despatch-envelop-controller");
 
 app.use(cors());
 app.use(express.json());
@@ -17,14 +17,10 @@ app.get("/", (req, res) =>
 
 app.post("/create-user");
 
-app.post("/create-user", (req, res) => userService.create(req, res));
+app.use(userService);
 
-app.post("/create-despatch-envelop", (req, res) =>
-  despatchEnvelopService.create(req, res)
-);
+app.use(transitSlipService);
 
-app.post("/create-transit-slip", (req, res) =>
-  transitSlipService.create(req, res)
-);
+app.use(despatchEnvelopService);
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
