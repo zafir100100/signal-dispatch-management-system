1725387f5134d034aec513a61fab1b1e718f0c04
Zafir <zafir.alvi@northsouth.edu>
2022-07-31 01:52:25 +0600

    despatch envelop and transit slip done

--- repositories/despatch-envelop-repository.js
+++ repositories/despatch-envelop-repository.js
@@ -33,8 +33,177 @@ async function createDespatchEnvelop(req) {
   }
 }
 
+async function getDespatchEnvelopById(req) {
+  const output = new ResponseDto();
+  try {
+      const despatchEnvelop = await DespatchEnvelop.findOne({
+          where: {
+              id: req.body.id,
+          },
+      });
+
+      if (!despatchEnvelop) {
+          output.message = 'No despatch envelop found with the given id.';
+          output.statusCode = 404;
+          return output;
+      }
+
+      output.message = 'Despatch Envelop found with the given id.';
+      output.statusCode = 200;
+      output.payload = {
+          output: despatchEnvelop,
+      };
+      return output;
+  } catch (error) {
+      output.payload = {
+          errorDetails: error,
+      };
+
+      return output;
+  }
+}
+
+async function getDespatchEnvelopByLetterNo(req) {
+  const output = new ResponseDto();
+  try {
+      const despatchEnvelop = await DespatchEnvelop.findOne({
+          where: {
+            letter_no: req.body.letter_no,
+          },
+      });
+
+      if (!despatchEnvelop) {
+          output.message = 'No Despatch Envelop found with the given letter no.';
+          output.statusCode = 404;
+          return output;
+      }
+
+      output.message = 'Despatch Envelop found with the given letter no.';
+      output.statusCode = 200;
+      output.payload = {
+          output: despatchEnvelop,
+      };
+      return output;
+  } catch (error) {
+      output.payload = {
+          errorDetails: error,
+      };
+
+      return output;
+  }
+}
+
+async function deleteDespatchEnvelopById(req) {
+  const output = new ResponseDto();
+  try {
+      const result = await sequelize.transaction(async (t) => {
+          const despatchEnvelop = await DespatchEnvelop.findOne({
+              where: {
+                  id: req.body.id,
+              },
+              transaction: t,
+          });
+
+          if (!despatchEnvelop) {
+              output.message = 'No despatchEnvelop found with the given id.';
+              output.statusCode = 404;
+              return output;
+          }
+
+          await DespatchEnvelop.destroy({
+              where: {
+                  id: req.body.id,
+              },
+              transaction: t,
+          });
+
+          output.message = 'Despatch Envelop Deletion Successful.';
+          output.isSuccess = true;
+          output.statusCode = 200;
+          output.payload = {
+              output: despatchEnvelop,
+          };
+      });
+
+      return output;
+  } catch (error) {
+      output.payload = {
+          errorDetails: error,
+      };
+
+      return output;
+  }
+}
+
+async function updateDespatchEnvelopById(req) {
+  const output = new ResponseDto();
+  try {
+      const result = await sequelize.transaction(async (t) => {
+          const despatchEnvelop = await DespatchEnvelop.findOne({
+              where: {
+                  id: req.body.id,
+              },
+              transaction: t,
+          });
+
+          if (!despatchEnvelop) {
+              output.message = 'No despatch envelop found with the given id.';
+              output.statusCode = 404;
+              return output;
+          }
+
+          await DespatchEnvelop.update(req.body, {
+              where: {
+                  id: req.body.id,
+              },
+              transaction: t,
+          });
+
+          output.message = 'Despatch Envelop Update Successful.';
+          output.isSuccess = true;
+          output.statusCode = 200;
+          output.payload = {
+              output: despatchEnvelop,
+          };
+      });
+
+      return output;
+  } catch (error) {
+      output.payload = {
+          errorDetails: error,
+      };
+
+      return output;
+  }
+}
+
 despatchEnvelopRepository.create = async function (req, res) {
   const output = await createDespatchEnvelop(req);
   res.status(output.statusCode);
   res.send(output);
 };
+
+despatchEnvelopRepository.getById = async function (req, res) {
+  const output = await getDespatchEnvelopById(req);
+  res.status(output.statusCode);
+  res.send(output);
+};
+
+despatchEnvelopRepository.getByLetterNo = async function (req, res) {
+  const output = await getDespatchEnvelopByLetterNo(req);
+  res.status(output.statusCode);
+  res.send(output);
+};
+
+despatchEnvelopRepository.delete = async function (req, res) {
+  const output = await deleteDespatchEnvelopById(req);
+  res.status(output.statusCode);
+  res.send(output);
+};
+
+despatchEnvelopRepository.update = async function (req, res) {
+  const output = await updateDespatchEnvelopById(req);
+  res.status(output.statusCode);
+  res.send(output);
+};
+
--- repositories/transit-slip-repository.js
+++ repositories/transit-slip-repository.js
@@ -1,40 +1,205 @@
 const TransitSlip = require('../models/tables/transit_slip');
-const ResponseDto = require("../models/DTOs/ResponseDto");
-const sequelize = require("../utils/db-connection");
+const ResponseDto = require('../models/DTOs/ResponseDto');
+const sequelize = require('../utils/db-connection');
 
 const transitSlipRepository = (module.exports = {});
 
 async function createTransitSlip(req) {
-  const output = new ResponseDto();
-  try {
-    const result = await sequelize.transaction(async (t) => {
-      const maxId = ((await TransitSlip.max("id")) ?? 0) + 1;
-      req.body.id = maxId;
-      const user = await TransitSlip.create(
-        req.body,
-        { transaction: t }
-      );
+    const output = new ResponseDto();
+    try {
+        const result = await sequelize.transaction(async (t) => {
+            const maxId = ((await TransitSlip.max('id')) ?? 0) + 1;
+            req.body.id = maxId;
+            const transitSlip = await TransitSlip.create(req.body, { transaction: t });
 
-      output.message = "Transit Slip Creation Successful.";
-      output.isSuccess = true;
-      output.statusCode = 200;
-      output.payload = {
-        output: user,
-      };
-    });
+            output.message = 'Transit Slip Creation Successful.';
+            output.isSuccess = true;
+            output.statusCode = 200;
+            output.payload = {
+                output: transitSlip,
+            };
+        });
 
-    return output;
-  } catch (error) {
-    output.payload = {
-      errorDetails: error,
-    };
+        return output;
+    } catch (error) {
+        output.payload = {
+            errorDetails: error,
+        };
 
-    return output;
-  }
+        return output;
+    }
+}
+
+async function getTransitSlipById(req) {
+    const output = new ResponseDto();
+    try {
+        const transitSlip = await TransitSlip.findOne({
+            where: {
+                id: req.body.id,
+            },
+        });
+
+        if (!transitSlip) {
+            output.message = 'No transit Slip found with the given id.';
+            output.statusCode = 404;
+            return output;
+        }
+
+        output.message = 'Transit Slip found with the given id.';
+        output.statusCode = 200;
+        output.payload = {
+            output: transitSlip,
+        };
+        return output;
+    } catch (error) {
+        output.payload = {
+            errorDetails: error,
+        };
+
+        return output;
+    }
+}
+
+async function getTransitSlipBySlipNo(req) {
+    const output = new ResponseDto();
+    try {
+        const transitSlip = await TransitSlip.findOne({
+            where: {
+                transit_slip_no: req.body.transit_slip_no,
+            },
+        });
+
+        if (!transitSlip) {
+            output.message = 'No transit Slip found with the given transit slip no.';
+            output.statusCode = 404;
+            return output;
+        }
+
+        output.message = 'Transit Slip found with the given transit slip no.';
+        output.statusCode = 200;
+        output.payload = {
+            output: transitSlip,
+        };
+        return output;
+    } catch (error) {
+        output.payload = {
+            errorDetails: error,
+        };
+
+        return output;
+    }
+}
+
+async function deleteTransitSlipById(req) {
+    const output = new ResponseDto();
+    try {
+        const result = await sequelize.transaction(async (t) => {
+            const transitSlip = await TransitSlip.findOne({
+                where: {
+                    id: req.body.id,
+                },
+                transaction: t,
+            });
+
+            if (!transitSlip) {
+                output.message = 'No transitSlip found with the given id.';
+                output.statusCode = 404;
+                return output;
+            }
+
+            await TransitSlip.destroy({
+                where: {
+                    id: req.body.id,
+                },
+                transaction: t,
+            });
+
+            output.message = 'Transit Slip Deletion Successful.';
+            output.isSuccess = true;
+            output.statusCode = 200;
+            output.payload = {
+                output: transitSlip,
+            };
+        });
+
+        return output;
+    } catch (error) {
+        output.payload = {
+            errorDetails: error,
+        };
+
+        return output;
+    }
+}
+
+async function updateTransitSlipById(req) {
+    const output = new ResponseDto();
+    try {
+        const result = await sequelize.transaction(async (t) => {
+            const transitSlip = await TransitSlip.findOne({
+                where: {
+                    id: req.body.id,
+                },
+                transaction: t,
+            });
+
+            if (!transitSlip) {
+                output.message = 'No transit slip found with the given id.';
+                output.statusCode = 404;
+                return output;
+            }
+
+            await TransitSlip.update(req.body, {
+                where: {
+                    id: req.body.id,
+                },
+                transaction: t,
+            });
+
+            output.message = 'Transit Slip Update Successful.';
+            output.isSuccess = true;
+            output.statusCode = 200;
+            output.payload = {
+                output: transitSlip,
+            };
+        });
+
+        return output;
+    } catch (error) {
+        output.payload = {
+            errorDetails: error,
+        };
+
+        return output;
+    }
 }
 
 transitSlipRepository.create = async function (req, res) {
-  const output = await createTransitSlip(req);
-  res.status(output.statusCode);
-  res.send(output);
+    const output = await createTransitSlip(req);
+    res.status(output.statusCode);
+    res.send(output);
+};
+
+transitSlipRepository.getById = async function (req, res) {
+    const output = await getTransitSlipById(req);
+    res.status(output.statusCode);
+    res.send(output);
+};
+
+transitSlipRepository.getBySlipNo = async function (req, res) {
+    const output = await getTransitSlipBySlipNo(req);
+    res.status(output.statusCode);
+    res.send(output);
+};
+
+transitSlipRepository.delete = async function (req, res) {
+    const output = await deleteTransitSlipById(req);
+    res.status(output.statusCode);
+    res.send(output);
+};
+
+transitSlipRepository.update = async function (req, res) {
+    const output = await updateTransitSlipById(req);
+    res.status(output.statusCode);
+    res.send(output);
 };
