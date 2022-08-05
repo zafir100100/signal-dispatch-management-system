d17e6a3af082e0b4bcee5447afb61e9fb189b9a2
Zafir <zafir.alvi@northsouth.edu>
2022-08-05 22:32:52 +0600

    new service added fr dedis

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const cors = require('cors');
 const userService = require('./controllers/user-info-controller');
 const transitSlipService = require('./controllers/transit-slip-controller');
 const despatchEnvelopService = require('./controllers/despatch-envelop-controller');
+const despatchEnvelopDistributionService = require('./controllers/despatch-envelop-distribution-controller');
 
 app.use(cors());
 app.use(express.json());
@@ -22,5 +23,7 @@ app.use(userService);
 app.use(transitSlipService);
 
 app.use(despatchEnvelopService);
+
+app.use(despatchEnvelopDistributionService);
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
